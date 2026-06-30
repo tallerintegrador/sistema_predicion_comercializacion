5c83f555af531f5bc33713760ae0e9c7ae9f216a
Valentin Fernandez <valenferxux@gmail.com>
2026-06-30 04:20:36 -0500

    feat: deploy frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,7 +15,7 @@ import { AuthProvider } from './auth/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <App />
       </AuthProvider>
